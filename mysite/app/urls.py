5b45e6b8eb932001196e96d59229a57ebfc7e220
flouMicaza <flo.mirandai@gmail.com>
2017-05-25 15:19:26 -0400

    funciona

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -18,4 +18,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.login, name='login'),
+
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -4,4 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'app/vendedor-profile-page.html')
+    return render(request, 'app/base.html')
+
+def login(request):
+    return render(request, 'app/login.html')
