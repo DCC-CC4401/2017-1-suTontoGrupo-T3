62784a30815deab08c292c2b0f44ebb6a95d43ec
flouMicaza <flo.mirandai@gmail.com>
2017-05-26 15:04:42 -0400

    empezando el modelo

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -2,5 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                )
+    tipo = models.CharField(max_length=50)
+
+
+class Vendedor(models.Model):
+    user = models.OneToOneField(UserInfo,
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                )
+class Ambulante(Vendedor):
+    check_in = models.
