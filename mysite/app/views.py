f39c909028f691543bf8292d96b0219fb96190de
alv-17 <alv-17@hotmail.com>
2017-05-29 04:44:27 -0400

    Base de datos

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -137,7 +137,7 @@ def get_menus(user):
 def vendedor_profile(request):
     usuario = UserInfo.objects.get(is_active=1)
     user = User.objects.get(username=usuario.user)
-    info_producto = {'menus' : get_menus(user.username),'usuario':usuario,'user':user}
+    info_producto = {'menus': get_menus("ratatouille"), 'usuario': usuario, 'user': user, }
     return render(request, 'app/vendedor_profile.html', context=info_producto)
 
 
